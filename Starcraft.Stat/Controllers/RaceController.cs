ba2e71a92b3b140b308d42c6cce4fd63875c1747
Grigory Zhadko <gregzhadko@gmail.com>
2021-11-26 00:31:45 +0700

    Added RaceController

---
+++ Starcraft.Stat/Controllers/RaceController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Starcraft.Stat.DataBase;
+using Starcraft.Stat.Models;
+
+namespace Starcraft.Stat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RaceController : ControllerBase
+    {
+        private readonly StarcraftDbContext _context;
+
+        public RaceController(StarcraftDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Race>>> GetRace()
+        {
+            return await _context.Race.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Race>> GetRace(string id)
+        {
+            var race = await _context.Race.FindAsync(id);
+
+            if (race == null)
+            {
+                return NotFound();
+            }
+
+            return race;
+        }
+    }
+}
