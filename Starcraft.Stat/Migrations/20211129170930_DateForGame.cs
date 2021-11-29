95ca0a37d0d6aa5a5f5e969af41d5f03953def82
Grigory Zhadko <gregzhadko@gmail.com>
2021-11-30 00:10:40 +0700

    Added Date to Game

---
+++ Starcraft.Stat/Migrations/20211129170930_DateForGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Starcraft.Stat.Migrations
+{
+    public partial class DateForGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "Games",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Games");
+        }
+    }
+}
