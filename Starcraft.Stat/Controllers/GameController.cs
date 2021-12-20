2ec05386df7c1bc3f2a03bb05b7768f200c83451
Grigory Zhadko <gregzhadko@gmail.com>
2021-12-20 13:51:16 +0700

    Moved game logic to game service

--- Starcraft.Stat/Controllers/GameController.cs
+++ Starcraft.Stat/Controllers/GameController.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Starcraft.Stat.DataBase;
-using Starcraft.Stat.DbModels;
 using Starcraft.Stat.Models.Requests;
 using Starcraft.Stat.Services;
 
@@ -11,32 +8,23 @@ namespace Starcraft.Stat.Controllers;
 [ApiController]
 public class GameController : ControllerBase
 {
-    private readonly StarcraftDbContext _context;
     private readonly IStatisticsService _statisticsService;
+    private readonly IGameService _gameService;
 
-    public GameController(StarcraftDbContext context, IStatisticsService statisticsService)
+    public GameController(IGameService gameService, IStatisticsService statisticsService)
     {
-        _context = context;
         _statisticsService = statisticsService;
+        _gameService = gameService;
     }
 
     [HttpPost("Add")]
-    public async Task<IActionResult> AddGame(AddGameRequest request)
+    public async Task<IActionResult> AddGame([FromQuery]bool pretty, [FromBody]AddGameRequest request)
     {
         try
         {
-            var races = (await _context.Races.ToArrayAsync()).ToDictionary(r => r.Name, r => r);
-            var players = (await _context.Players.ToArrayAsync()).ToDictionary(p => p.Name, p => p);
-
-            var team1 = await GetExistingTeamAsync(BuildTeam(request.Team1, races, players));
-            var team2 = await GetExistingTeamAsync(BuildTeam(request.Team2, races, players));
-
-            var game = new Game
-                {Team1 = team1, Team2 = team2, Winner = request.Winner, Date = DateOnly.FromDateTime(DateTime.UtcNow)};
-            _context.Games.Add(game);
-            await _context.SaveChangesAsync();
-
-            return Ok(await _statisticsService.GetPlayerStatisticsAsync());
+            await _gameService.AddGameAsync(request);
+            var response = await _statisticsService.GetPlayerStatisticsAsync();
+            return Ok(pretty ? response.ToPretty() : response);
         }
         catch
         {
@@ -44,29 +32,5 @@ public class GameController : ControllerBase
         }
     }
 
-    private static Team BuildTeam(TeamRequest request, IReadOnlyDictionary<string, Race> races,
-        IReadOnlyDictionary<string, Player> players)
-    {
-        return new Team
-        {
-            Player1Id = players[request.Player1].Id,
-            Race1Id = races[request.Race1].Name,
-            Player2Id = players[request.Player2].Id,
-            Race2Id = races[request.Race2].Name
-        };
-    }
 
-    private async Task<Team> GetExistingTeamAsync(Team team)
-    {
-        var existingTeam = await _context.Teams.FirstOrDefaultAsync(t =>
-            t.Player1Id == team.Player1Id && t.Race1Id == team.Race1Id && t.Player2Id == team.Player2Id &&
-            t.Race2Id == team.Race2Id);
-        if (existingTeam != null)
-        {
-            return existingTeam;
-        }
-
-        _context.Teams.Add(team);
-        return team;
-    }
 }
