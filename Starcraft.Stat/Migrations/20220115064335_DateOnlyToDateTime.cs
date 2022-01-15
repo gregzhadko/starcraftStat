23f450ab456e304e8bbee2676b3409d17eace1cd
Grigory Zhadko <gregzhadko@gmail.com>
2022-01-15 16:30:13 +0700

    #1 DateOnly to Date in Game table

---
+++ Starcraft.Stat/Migrations/20220115064335_DateOnlyToDateTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Starcraft.Stat.Migrations
+{
+    public partial class DateOnlyToDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Games",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "Games",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
