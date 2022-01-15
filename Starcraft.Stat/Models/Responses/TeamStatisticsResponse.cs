83ed4c559b0697db7e92cbf2e3df069a9c28f2aa
Grigory Zhadko <gregzhadko@gmail.com>
2022-01-15 17:58:17 +0700

    #1 Moved responses to separate classes

---
+++ Starcraft.Stat/Models/Responses/TeamStatisticsResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Starcraft.Stat.Models.Responses;
+
+public record TeamStatisticsResponse(string Player1, string Player2, int Wins) : IPretty
+{
+    public static string Header => $"{"Wins",-5}Races";
+    public string ToPretty() => $"{$"{Wins}",-5}{$"{Player1}",-15}{Player2}";
+}
