9a5181f561d20686bc6a10328fb2d9e729106baf
Grigory Zhadko <gregzhadko@gmail.com>
2022-01-18 14:11:13 +0700

    Added Losses and Win Rate to players and teams statistics

--- Starcraft.Stat/Models/Responses/TeamStatisticsResponse.cs
+++ Starcraft.Stat/Models/Responses/TeamStatisticsResponse.cs
@@ -1,7 +1,7 @@
 ﻿namespace Starcraft.Stat.Models.Responses;
 
-public record TeamStatisticsResponse(string Player1, string Player2, int Wins) : IPretty
+public record TeamStatisticsResponse(string Player1, string Player2, int Wins, int Losses, double WinRate) : IPretty
 {
-    public static string Header => $"{"Wins",-5}Teams";
-    public string ToPretty() => $"{$"{Wins}",-5}{$"{Player1}",-15}{Player2}";
+    public static string Header => $"{"Wins",-5}{"Losses",-7}{"Win Rate",-11}Teams";
+    public string ToPretty() => $"{$"{Wins}",-5}{$"{Losses}",-7}{$"{WinRate:N}",-11}{$"{Player1}",-15}{Player2}";
 }
