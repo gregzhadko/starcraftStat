8c44dd58847838923d88a17f29b90137116f0599
Grigory Zhadko <gregzhadko@gmail.com>
2022-01-27 20:21:04 +0700

    #5 Attempt to fix deployment for telegram bot

--- Starcraft.Stat/Program.cs
+++ Starcraft.Stat/Program.cs
@@ -17,28 +17,28 @@ var services = builder.Services;
 // There are several strategies for completing asynchronous tasks during startup.
 // Some of them could be found in this article https://andrewlock.net/running-async-tasks-on-app-startup-in-asp-net-core-part-1/
 // We are going to use IHostedService to add and later remove Webhook
-services.AddHostedService<ConfigureWebhook>();
+//services.AddHostedService<ConfigureWebhook>();
 
 // Register named HttpClient to get benefits of IHttpClientFactory
 // and consume it with ITelegramBotClient typed client.
 // More read:
 //  https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-5.0#typed-clients
 //  https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/use-httpclientfactory-to-implement-resilient-http-requests
-services.AddHttpClient("telegramWebHook")
-    .AddTypedClient<ITelegramBotClient>(httpClient
-        => new TelegramBotClient(botConfig.BotToken, httpClient));
+// services.AddHttpClient("telegramWebHook")
+//     .AddTypedClient<ITelegramBotClient>(httpClient
+//         => new TelegramBotClient(botConfig.BotToken, httpClient));
 
 services.AddDbContext<StarcraftDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("StarcraftDbContext")));
 
-services.AddScoped<IBotHandleService, BotHandleService>();
+//services.AddScoped<IBotHandleService, BotHandleService>();
 services.AddScoped<IStatisticsService, StatisticsService>();
 services.AddScoped<IGameService, GameService>();
 
 // Add services to the container.
 
 services.AddControllers()
-    .AddNewtonsoftJson()
+    //.AddNewtonsoftJson()
     .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AddGameRequestValidator>());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -65,24 +65,24 @@ Console.WriteLine("Starcraft started");
 
 app.UseHttpsRedirection();
 //app.UseAuthorization();
-app.UseRouting();
-app.UseCors();
+// app.UseRouting();
+// app.UseCors();
 
 //app.MapControllers();
 
-app.UseEndpoints(endpoints =>
-{
-    // Configure custom endpoint per Telegram API recommendations:
-    // https://core.telegram.org/bots/api#setwebhook
-    // If you'd like to make sure that the Webhook request comes from Telegram, we recommend
-    // using a secret path in the URL, e.g. https://www.example.com/<token>.
-    // Since nobody else knows your bot's token, you can be pretty sure it's us.
-    var token = botConfig.BotToken;
-    endpoints.MapControllerRoute("telegramWebHook",
-        $"bot/{token}",
-        new { controller = "Webhook", action = "Post" });
-    endpoints.MapControllers();
-});
+// app.UseEndpoints(endpoints =>
+// {
+//     // Configure custom endpoint per Telegram API recommendations:
+//     // https://core.telegram.org/bots/api#setwebhook
+//     // If you'd like to make sure that the Webhook request comes from Telegram, we recommend
+//     // using a secret path in the URL, e.g. https://www.example.com/<token>.
+//     // Since nobody else knows your bot's token, you can be pretty sure it's us.
+//     var token = botConfig.BotToken;
+//     endpoints.MapControllerRoute("telegramWebHook",
+//         $"bot/{token}",
+//         new { controller = "Webhook", action = "Post" });
+//     endpoints.MapControllers();
+// });
 
 app.MapControllers();
 
