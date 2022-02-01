cf68e09bb78038dca14bb8f2c748444a3839bbea
Grigory Zhadko <gregzhadko@gmail.com>
2022-02-01 22:20:57 +0700

    Renaming ConfigureWebhook to WebhookService

--- Starcraft.Stat/Services/WebhookService.cs
+++ Starcraft.Stat/Services/WebhookService.cs
@@ -4,15 +4,13 @@ using Telegram.Bot.Types.Enums;
 
 namespace Starcraft.Stat.Services;
 
-public class ConfigureWebhook : IHostedService
+public class WebhookService : IHostedService
 {
     private readonly BotConfiguration _botConfig;
-    private readonly ILogger<ConfigureWebhook> _logger;
+    private readonly ILogger<WebhookService> _logger;
     private readonly IServiceProvider _services;
 
-    public ConfigureWebhook(ILogger<ConfigureWebhook> logger,
-        IServiceProvider serviceProvider,
-        IConfiguration configuration)
+    public WebhookService(ILogger<WebhookService> logger, IServiceProvider serviceProvider, IConfiguration configuration)
     {
         _logger = logger;
         _services = serviceProvider;
@@ -24,17 +22,9 @@ public class ConfigureWebhook : IHostedService
         using var scope = _services.CreateScope();
         var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
 
-        // Configure custom endpoint per Telegram API recommendations:
-        // https://core.telegram.org/bots/api#setwebhook
-        // If you'd like to make sure that the Webhook request comes from Telegram, we recommend
-        // using a secret path in the URL, e.g. https://www.example.com/<token>.
-        // Since nobody else knows your bot's token, you can be pretty sure it's us.
         var webhookAddress = @$"{_botConfig.HostAddress}/bot/{_botConfig.BotToken}";
         _logger.LogInformation("Setting webhook: {WebhookAddress}", webhookAddress);
-        await botClient.SetWebhookAsync(
-            webhookAddress,
-            allowedUpdates: Array.Empty<UpdateType>(),
-            cancellationToken: cancellationToken);
+        await botClient.SetWebhookAsync(webhookAddress, allowedUpdates: Array.Empty<UpdateType>(), cancellationToken: cancellationToken);
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
