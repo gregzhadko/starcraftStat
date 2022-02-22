9653944ce9d30122bb684b3250406e99ea286009
Grigory Zhadko <gregzhadko@gmail.com>
2022-02-22 21:07:02 +0700

    Simplified compare method

--- Starcraft.Stat/Models/Responses/WinLoseBase.cs
+++ Starcraft.Stat/Models/Responses/WinLoseBase.cs
@@ -3,7 +3,7 @@
 public record WinLoseBase(int Wins, int Losses, double WinRate) : IPretty
 {
     public int Games => Wins + Losses;
-    public static string Header => $"{"Wins",-5}{"Losses",-7}{"Games",-6}{"Win Rate",-11}";
+    protected static string Header => $"{"Wins",-5}{"Losses",-7}{"Games",-6}{"Win Rate",-11}";
     public virtual string ToPretty() => $"{$"{Wins}",-5}{$"{Losses}",-7}{$"{Games}",-6}{$"{WinRate:N}",-11}";
 }
 
@@ -15,35 +15,28 @@ public class WinLoseComparer : IComparer<WinLoseBase>
         {
             return 1;
         }
-        else if(x.WinRate < y.WinRate)
+
+        if(x.WinRate < y.WinRate)
         {
             return -1;
         }
-        else
+
+        if (x.Wins >= x.Losses) //Если WinRate больше или равен 50 процентов, то результаты с большим количеством игр должны быть в выше в списке
         {
-            if (x.Wins >= x.Losses) //Если WinRate больше или равен 50 процентов, то результаты с большим количеством игр должны быть в выше в списке
+            if (x.Games >= y.Games)
             {
-                if (x.Games >= y.Games)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return -1;
-                }
+                return 1;
             }
-            else //Иначе результы с наименьшим количеством игр должны быть ниже
-            {
-                if (x.Games >= y.Games)
-                {
-                    return -1;
-                }
-                else
-                {
-                    return 1;
-                }
-            }
+
+            return -1;
         }
-        
+
+        //Иначе результы с наименьшим количеством игр должны быть ниже
+        if (x.Games >= y.Games)
+        {
+            return -1;
+        }
+
+        return 1;
     }
 }
