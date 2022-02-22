cd9c6b9bb8e48b1ef24c78e0dacc9b8a516fc593
Grigory Zhadko <gregzhadko@gmail.com>
2022-02-22 18:25:10 +0700

    Made statistics response prettier

--- Starcraft.Stat/Models/Responses/TeamPlayerRaceResponse.cs
+++ Starcraft.Stat/Models/Responses/TeamPlayerRaceResponse.cs
@@ -2,6 +2,6 @@
 
 public record TeamPlayerRaceResponse(string Player1, string Race1, string Player2, string Race2, int Wins, int Losses, double WinRate) : IPretty
 {
-    public static string Header => $"{"Wins",-5}{"Losses",-7}{"Win Rate",-11}{"Player1",-15}{"Race1",-5}{"Player2",-15}{"Race2",-5}";
-    public string ToPretty() => $"{$"{Wins}",-5}{$"{Losses}",-7}{$"{WinRate:N}",-11}{$"{Player1}",-15}{Race1,-5}{$"{Player2}",-15}{Race2,-5}";
+    public static string Header => $"{"Wins",-5}{"Losses",-7}{"Win Rate",-11}{"Player1",-15}{"Race1",-15}{"Player2",-15}{"Race2",-5}";
+    public string ToPretty() => $"{$"{Wins}",-5}{$"{Losses}",-7}{$"{WinRate:N}",-11}{$"{Player1}",-15}{Race1,-15}{$"{Player2}",-15}{Race2,-5}";
 }
