298a606fb32fd90b36ed3242732030c01c2ea498
Grigory Zhadko <gregzhadko@gmail.com>
2022-03-16 23:10:29 +0700

    Removed unused methods from BotHandleService

--- Starcraft.Stat/Services/BotHandleService.cs
+++ Starcraft.Stat/Services/BotHandleService.cs
@@ -5,8 +5,6 @@ using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
-using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Starcraft.Stat.Services;
@@ -106,88 +104,6 @@ public class BotHandleService : IBotHandleService
 
         var sentMessage = await action;
         _logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
-
-        // Send inline keyboard
-        // You can process responses in BotOnCallbackQueryReceived handler
-        static async Task<Message> SendInlineKeyboard(ITelegramBotClient bot, Message message)
-        {
-            await bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
-
-            // Simulate longer running task
-            await Task.Delay(500);
-
-            InlineKeyboardMarkup inlineKeyboard = new(
-                new[]
-                {
-                    // first row
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("1.1", "11"),
-                        InlineKeyboardButton.WithCallbackData("1.2", "12")
-                    },
-                    // second row
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("2.1", "21"),
-                        InlineKeyboardButton.WithCallbackData("2.2", "22")
-                    }
-                });
-
-            return await bot.SendTextMessageAsync(message.Chat.Id,
-                "Choose",
-                replyMarkup: inlineKeyboard);
-        }
-
-        static async Task<Message> SendReplyKeyboard(ITelegramBotClient bot, Message message)
-        {
-            ReplyKeyboardMarkup replyKeyboardMarkup = new(
-                new[]
-                {
-                    new KeyboardButton[] { "1.1", "1.2" },
-                    new KeyboardButton[] { "2.1", "2.2" }
-                })
-            {
-                ResizeKeyboard = true
-            };
-
-            return await bot.SendTextMessageAsync(message.Chat.Id,
-                "Choose",
-                replyMarkup: replyKeyboardMarkup);
-        }
-
-        static async Task<Message> RemoveKeyboard(ITelegramBotClient bot, Message message)
-        {
-            return await bot.SendTextMessageAsync(message.Chat.Id,
-                "Removing keyboard",
-                replyMarkup: new ReplyKeyboardRemove());
-        }
-
-        static async Task<Message> SendFile(ITelegramBotClient bot, Message message)
-        {
-            await bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
-
-            const string filePath = @"Files/tux.png";
-            await using FileStream fileStream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-            var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
-
-            return await bot.SendPhotoAsync(message.Chat.Id,
-                new InputOnlineFile(fileStream, fileName),
-                "Nice Picture");
-        }
-
-        static async Task<Message> RequestContactAndLocation(ITelegramBotClient bot, Message message)
-        {
-            ReplyKeyboardMarkup requestReplyKeyboard = new(
-                new[]
-                {
-                    KeyboardButton.WithRequestLocation("Location"),
-                    KeyboardButton.WithRequestContact("Contact")
-                });
-
-            return await bot.SendTextMessageAsync(message.Chat.Id,
-                "Who or Where are you?",
-                replyMarkup: requestReplyKeyboard);
-        }
     }
 
     private async Task<Message> AddGameAsync(Message message)
@@ -235,13 +151,6 @@ public class BotHandleService : IBotHandleService
         return await _botClient.SendTextMessageAsync(message.Chat.Id, helpText, replyMarkup: new ReplyKeyboardRemove());
     }
 
-    // Process Inline Keyboard callback data
-    private async Task BotOnCallbackQueryReceived(CallbackQuery callbackQuery)
-    {
-        await _botClient.AnswerCallbackQueryAsync(callbackQuery.Id, $"Received {callbackQuery.Data}");
-        await _botClient.SendTextMessageAsync(callbackQuery.Message!.Chat.Id, $"Received {callbackQuery.Data}");
-    }
-
     private Task UnknownUpdateHandlerAsync(Update update)
     {
         _logger.LogInformation("Unknown update type: {UpdateType}", update.Type);
@@ -259,36 +168,4 @@ public class BotHandleService : IBotHandleService
         _logger.LogInformation("HandleError: {ErrorMessage}", errorMessage);
         await _botClient.SendTextMessageAsync(chatIt, $"Грег наговнокодил: {exception.Message}");
     }
-
-    #region Inline Mode
-
-    private async Task BotOnInlineQueryReceived(InlineQuery inlineQuery)
-    {
-        _logger.LogInformation("Received inline query from: {InlineQueryFromId}", inlineQuery.From.Id);
-
-        InlineQueryResult[] results =
-        {
-            // displayed result
-            new InlineQueryResultArticle(
-                "3",
-                "TgBots",
-                new InputTextMessageContent(
-                    "hello"
-                )
-            )
-        };
-
-        await _botClient.AnswerInlineQueryAsync(inlineQuery.Id,
-            results,
-            isPersonal: true,
-            cacheTime: 0);
-    }
-
-    private Task BotOnChosenInlineResultReceived(ChosenInlineResult chosenInlineResult)
-    {
-        _logger.LogInformation("Received inline result: {ChosenInlineResultId}", chosenInlineResult.ResultId);
-        return Task.CompletedTask;
-    }
-
-    #endregion
 }
