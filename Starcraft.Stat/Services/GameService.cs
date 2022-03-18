d4f6070b4c477412c21e038b37f14a9d8ab1959a
Grigory Zhadko <gregzhadko@gmail.com>
2022-03-18 13:28:28 +0700

    Attempt to use Throw nuget

--- Starcraft.Stat/Services/GameService.cs
+++ Starcraft.Stat/Services/GameService.cs
@@ -3,6 +3,7 @@ using Starcraft.Stat.DataBase;
 using Starcraft.Stat.DbModels;
 using Starcraft.Stat.Exceptions;
 using Starcraft.Stat.Models.Requests;
+using Throw;
 
 namespace Starcraft.Stat.Services;
 
@@ -34,28 +35,16 @@ public class GameService : IGameService
     private static Team BuildTeam(TeamRequest request, IReadOnlyCollection<Race> races, IReadOnlyCollection<Player> players)
     {
         var race1Id = races.FirstOrDefault(r => r.Name.StartsWith(request.Race1, StringComparison.InvariantCultureIgnoreCase))?.Name;
-        if (race1Id == null)
-        {
-            throw new StarcraftException($"There is no race with '{request.Race1}' name");
-        }
-
+        race1Id.ThrowIfNull(_ => new StarcraftException($"There is no race with '{request.Race1}' name"));
+        
         var race2Id = races.FirstOrDefault(r => r.Name.StartsWith(request.Race2, StringComparison.InvariantCultureIgnoreCase))?.Name;
-        if (race2Id == null)
-        {
-            throw new StarcraftException($"There is no race with '{request.Race2}' name");
-        }
+        race2Id.ThrowIfNull(_ => throw new StarcraftException($"There is no race with '{request.Race2}' name"));
 
         var player1Id = players.FirstOrDefault(r => r.Name.StartsWith(request.Player1, StringComparison.InvariantCultureIgnoreCase))?.Id;
-        if (player1Id == null)
-        {
-            throw new StarcraftException($"There is no player with '{request.Player1}' name");
-        }
+        player1Id.ThrowIfNull(_ => throw new StarcraftException($"There is no player with '{request.Player1}' name"));
 
         var player2Id = players.FirstOrDefault(r => r.Name.StartsWith(request.Player2, StringComparison.InvariantCultureIgnoreCase))?.Id;
-        if (player2Id == null)
-        {
-            throw new StarcraftException($"There is no player with '{request.Player2}' name");
-        }
+        player2Id.ThrowIfNull(_ => throw new StarcraftException($"There is no player with '{request.Player2}' name"));
 
         return new Team
         {
