8e3ff51d07f5ba45652deee5a71aadd28e5ceaf9
gregzhadko <gregzhadko@gmail.com>
2023-12-29 21:34:03 +0100

    Updating all nuget packages and fix errors after that

--- Starcraft.Stat/Services/BotHandleService.cs
+++ Starcraft.Stat/Services/BotHandleService.cs
@@ -55,7 +55,7 @@ public class BotHandleService(ITelegramBotClient botClient, ILogger logger, ISta
 
     private async Task HandleStarcraftExceptionAsync(StarcraftException starcraftException, long chatId)
     {
-        await botClient.SendTextMessageAsync(chatId, starcraftException.Message, ParseMode.Markdown);
+        await botClient.SendTextMessageAsync(chatId, starcraftException.Message, parseMode: ParseMode.Markdown);
     }
 
     private async Task BotOnMessageReceived(Message message)
@@ -98,30 +98,30 @@ public class BotHandleService(ITelegramBotClient botClient, ILogger logger, ISta
         if (allowedChats.Length > 0 && !allowedChats.Contains(message.Chat.Id))
         {
             logger.Warning("Someone tried to send {Message} from chat {Chat}, but we didn't allowed it", message.Text, message.Chat.Id);
-            return await botClient.SendTextMessageAsync(message.Chat.Id, "Only Grigory and tstk chat can add games to the statistics", ParseMode.Markdown);
+            return await botClient.SendTextMessageAsync(message.Chat.Id, "Only Grigory and tstk chat can add games to the statistics", parseMode: ParseMode.Markdown);
         }
 
         var s = message.Text!.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
         if (s.Length < 9)
         {
-            return await botClient.SendTextMessageAsync(message.Chat.Id, $"Not enough parameters\\. The correct format is `{AddFormat}`", ParseMode.MarkdownV2);
+            return await botClient.SendTextMessageAsync(message.Chat.Id, $"Not enough parameters\\. The correct format is `{AddFormat}`", parseMode: ParseMode.MarkdownV2);
         }
 
         var request = new AddGameRequest(
-            new TeamRequest(s[1], s[2], s[3], s[4]),
-            new TeamRequest(s[5], s[6], s[7], s[8]),
+            new(s[1], s[2], s[3], s[4]),
+            new(s[5], s[6], s[7], s[8]),
             Winner.Team1,
             false);
         var validationResult = await new AddGameRequestValidator().ValidateAsync(request);
         if (!validationResult.IsValid)
         {
             var response = $"Validation errors:\n {string.Join("\n  ", validationResult.Errors.Select(e => e.ErrorMessage))}";
-            return await botClient.SendTextMessageAsync(message.Chat.Id, response, ParseMode.MarkdownV2);
+            return await botClient.SendTextMessageAsync(message.Chat.Id, response, parseMode: ParseMode.MarkdownV2);
         }
 
         await gameService.AddGameAsync(request);
         var number = await gameService.GetGamesCountAsync();
-        await botClient.SendTextMessageAsync(message.Chat.Id, $"Game №{number} is added", ParseMode.MarkdownV2);
+        await botClient.SendTextMessageAsync(message.Chat.Id, $"Game №{number} is added", parseMode: ParseMode.MarkdownV2);
         return await GetPrettyStatisticsAsync(message.Chat.Id);
     }
 
@@ -129,7 +129,7 @@ public class BotHandleService(ITelegramBotClient botClient, ILogger logger, ISta
     {
         var statistics = await statisticsService.GetPlayerStatisticsAsync(false, false);
         var result = $"`{statistics.ToPretty()}`";
-        return await botClient.SendTextMessageAsync(chatId, result, ParseMode.MarkdownV2);
+        return await botClient.SendTextMessageAsync(chatId, result, parseMode: ParseMode.MarkdownV2);
     }
 
     private async Task<Message> HelpAsync(Message message)
